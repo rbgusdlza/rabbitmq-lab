7f2d8a35813ec11c0a52867e3073e37d03fbd4de
rbgusdlza <rbgusdlza@naver.com>
2024-09-16 00:45:06 +0900

    init

---
+++ src/main/java/rbgusdlza/rabbitmqlab/RabbitmqlabApplication.java
@@ -0,0 +1,13 @@
+package rbgusdlza.rabbitmqlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rbgusdlza/rabbitmqlab/RabbitmqlabApplicationTests.java
@@ -0,0 +1,13 @@
+package rbgusdlza.rabbitmqlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
